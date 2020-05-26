cba49382c700fe6594c5ecacb127eae26c1b1d57
dimitrije97 <dimitrijesalic@yahoo.com>
2020-05-26 02:44:06 +0200

    Login and registration forms with username and password validation

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  validateForm: FormGroup;
+
+  constructor(private route: ActivatedRoute, private message: NzMessageService, private fb: FormBuilder, private router: Router, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      username: [null, [Validators.required, Validators.email, Validators.minLength(8)]],
+      password: [null, [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]]
+    });
+  }
+
+  submitForm(): void {
+    // for (const i in this.validateForm.controls) {
+    //   this.validateForm.controls[i].markAsDirty();
+    //   this.validateForm.controls[i].updateValueAndValidity();
+    // }
+  }
+
+  onRegistration() {
+    this.router.navigateByUrl('auth/registration');
+  }
+}
---
+++ src/app/services/registration-request.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationRequestService {
+
+  private baseUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+}
